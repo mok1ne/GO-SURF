29519f0e0fd11b439069bd6be71b4233d436aed4
Danymag <daniil.magula@gmail.com>
2024-11-12 01:22:06 +0300

    finnalheader

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,48 @@
 import '../../src/scss/style.scss'
 
+
+
 $('.bg__slider-info').slick({
     infinite: true,
-    speed: 2000,
+    speed: 1000,
     arrows: false,
     dots: true,
     dotsClass: 'slick-dots',
+    customPaging: function (slider, i) {
+        const dotNumber = (i + 1).toString().padStart(2, '0');
+        const dotNames = ['North Shore', 'South Shore', 'West Shore', 'East Shore'];
+        const dotName = dotNames[i];
+        return `<div class="dots__slider"><h3>${dotNumber}</h3><h4>${dotName}</h4></div>`;
+    },
     fade: true,
     autoplay: true,
-    autoplaySpeed: 8000,
+    autoplaySpeed: 7000,
     pauseOnFocus: false,
     pauseOnHover: false,
     swipe: true,
     draggable: false,
     zIndex: 10
 });
+
+
+function clockTick() {
+    let currentTime = new Date(),
+        month = currentTime.getMonth() + 1,
+        day = currentTime.getDate(),
+        year = currentTime.getFullYear()
+    const dayElement = document.querySelector('.header__info h2');
+    const dateAndYearElement = document.querySelector('.header__info h6');
+
+    dayElement.textContent = day;
+
+    dateAndYearElement.textContent = `${month} | ${year}`;
+}
+
+clockTick();
+
+
+
+
+
+
 
